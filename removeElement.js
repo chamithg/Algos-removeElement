623a32964273a6290c818dd05864757943187395
gamagegit <cgamage92@yahoo.com>
2022-10-22 00:11:50 -0700

    finished solution

--- removeElement.js
+++ removeElement.js
@@ -3,4 +3,19 @@
  * @param {number} val
  * @return {number}
  */
-var removeElement = function (nums, val) {};
+var removeElement = function (nums, val) {
+  let numLen = nums.length;
+
+  let i = 0;
+  while (i < nums.length) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      numLen--;
+      nums.push("-");
+    } else {
+      i++;
+    }
+  }
+
+  return numLen;
+};
